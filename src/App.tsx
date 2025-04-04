4fbc04633f16774a6ce14adbafe82484b8f33d03
zend1e <esimhanovanuria@gmail.com>
2025-04-04 14:54:35 +0500

    made zarina to zara

--- src/App.tsx
+++ src/App.tsx
@@ -103,7 +103,7 @@ function App() {
       avatar: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?w=150",
     },{
       id: 4,
-      name: "Zarina",
+      name: "Zara",
       subject: "Mathematics",
       avatar: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=150",
     },
